0967e77ea7f711e6bab07c84a5279ab176566000
Yanpi1 <yanpietro0101@gmail.com>
2026-08-12 17:09:22 -0300

    fix: corrige Bug - Pedido permite vender quantidade maior que o estoque disponivel (OS 004)

---
+++ backend/src/main/java/com/senac/loja/repository/ItemPedidoRepository.java
@@ -0,0 +1,7 @@
+package com.senac.loja.repository;
+
+import com.senac.loja.model.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
+}
